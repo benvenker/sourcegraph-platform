13a5766330af6b48f0c654538d36074caed8e096
Erik Seliger <erikseliger@me.com>
2022-06-06 02:26:35 +0200

    Fixups

--- cmd/frontend/graphqlbackend/batches.go
+++ cmd/frontend/graphqlbackend/batches.go
@@ -811,6 +811,7 @@ type BatchSpecWorkspaceResolver interface {
 	Unsupported() bool
 	DiffStat(ctx context.Context) (*DiffStat, error)
 	PlaceInQueue() *int32
+	PlaceInGlobalQueue() *int32
 
 	ToHiddenBatchSpecWorkspace() (HiddenBatchSpecWorkspaceResolver, bool)
 	ToVisibleBatchSpecWorkspace() (VisibleBatchSpecWorkspaceResolver, bool)
