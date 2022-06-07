f1f1d3a1daaf38444fca57889fa838b8422884b6
Philipp Spiess <hello@philippspiess.com>
2022-06-07 11:12:39 +0200

    JetBrains: Manually emit window closing/open events to fix third-party modules that rely on those to render overlays

--- client/jetbrains/src/main/java/com/sourcegraph/find/SourcegraphWindow.java
+++ client/jetbrains/src/main/java/com/sourcegraph/find/SourcegraphWindow.java
@@ -16,9 +16,9 @@ import org.cef.browser.CefBrowser;
 import org.cef.handler.CefKeyboardHandler;
 import org.cef.misc.BoolRef;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
-import java.awt.event.AWTEventListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowEvent;
 
@@ -29,6 +29,8 @@ public class SourcegraphWindow implements Disposable {
     private final Project project;
     private final FindPopupPanel mainPanel;
     private JBPopup popup;
+    @Nullable
+    private Window window;
     private static final Logger logger = Logger.getInstance(SourcegraphWindow.class);
 
     public SourcegraphWindow(@NotNull Project project) {
@@ -43,9 +45,20 @@ public class SourcegraphWindow implements Disposable {
             popup = createPopup();
             popup.showCenteredInCurrentWindow(project);
             registerOutsideClickListener();
+
+            if (popup instanceof AbstractPopup) {
+                window = ((AbstractPopup) popup).getPopupWindow();
+            }
+        } else {
+            popup.setUiVisible(true);
+
+            if (window != null) {
+                // Manually emit a WINDOW_OPENED event if we change the popup to make it visible again. This works around
+                // issues with third-party extensions like the material UI theme that uses these events to display overlays.
+                window.dispatchEvent(new WindowEvent(window, WindowEvent.WINDOW_OPENED));
+            }
         }
 
-        popup.setUiVisible(true);
 
         // If the popup is already shown, hitting alt + a gain should behave the same as the native find in files
         // feature and focus the search field.
@@ -56,6 +69,12 @@ public class SourcegraphWindow implements Disposable {
 
     public void hidePopup() {
         popup.setUiVisible(false);
+
+        if (window != null) {
+            // Manually emit a WINDOW_CLOSING event if we change the popup to make it invisible. This works around
+            // issues with third-party extensions like the material UI theme that uses these events to display overlays.
+            window.dispatchEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSING));
+        }
     }
 
     @NotNull
@@ -143,31 +162,24 @@ public class SourcegraphWindow implements Disposable {
     private void registerOutsideClickListener() {
         Window projectParentWindow = getParentWindow(null);
 
-        Toolkit.getDefaultToolkit().addAWTEventListener(new AWTEventListener() {
-            @Override
-            public void eventDispatched(AWTEvent event) {
-                if (event instanceof WindowEvent) {
-                    WindowEvent windowEvent = (WindowEvent) event;
+        Toolkit.getDefaultToolkit().addAWTEventListener(event -> {
+            if (event instanceof WindowEvent) {
+                WindowEvent windowEvent = (WindowEvent) event;
 
-                    // We only care for focus events
-                    if (windowEvent.getID() != WINDOW_GAINED_FOCUS) {
-                        return;
-                    }
+                // We only care for focus events
+                if (windowEvent.getID() != WINDOW_GAINED_FOCUS) {
+                    return;
+                }
 
-                    // Detect if we're focusing the Sourcegraph popup
-                    if (popup instanceof AbstractPopup) {
-                        Window sourcegraphPopupWindow = ((AbstractPopup) popup).getPopupWindow();
+                // Detect if we're focusing the Sourcegraph popup
+                if (windowEvent.getWindow().equals(window)) {
+                    return;
+                }
 
-                        if (windowEvent.getWindow().equals(sourcegraphPopupWindow)) {
-                            return;
-                        }
-                    }
-
-                    // Detect if the newly focused window is a parent of the project root window
-                    Window currentProjectParentWindow = getParentWindow(windowEvent.getComponent());
-                    if (currentProjectParentWindow.equals(projectParentWindow)) {
-                        hidePopup();
-                    }
+                // Detect if the newly focused window is a parent of the project root window
+                Window currentProjectParentWindow = getParentWindow(windowEvent.getComponent());
+                if (currentProjectParentWindow.equals(projectParentWindow)) {
+                    hidePopup();
                 }
             }
         }, AWTEvent.WINDOW_EVENT_MASK);
